4b43b49855f2e2767b26089ba4e6ab977de8c4f9
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 18:54:50 -0700

    Set Colors
    - Set list of colors after submit and color values retrieved from the library
    - The color section iterates through list of colors and creates a SingleColor component of each one for the UI

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App () {
     try {
       let colors = new Values(color).all(10);  
       setError(false);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -34,7 +35,9 @@ function App () {
         </form>
       </section>
 			<section className="colors">
-				<h4>list goes heasdfasdfre</h4>
+				{list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index}/>
+        })}
 			</section>
 		</>
 	);
